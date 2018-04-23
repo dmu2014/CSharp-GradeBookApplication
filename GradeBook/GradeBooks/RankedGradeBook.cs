b01da9d44aa1c4bddd3f6bc157a3b8221a09b399
dmu2014 <dishank2007@gmail.com>
2018-04-22 22:20:15 -0400

    Next changes

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GradeBook.Enums;
 
@@ -10,6 +11,23 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             this.Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (this.Students.Count < 5) throw new System.InvalidOperationException();
+            var studentsOrderedByGrades = this.Students.OrderByDescending(s => s.AverageGrade);
+            var studentCount = this.Students.Count;
+            var bucketWidth = (int)Math.Ceiling(0.2 * studentCount);
+            var cutoff_A = studentsOrderedByGrades.ElementAt(bucketWidth - 1).AverageGrade;
+            var cutoff_B = studentsOrderedByGrades.ElementAt(2*bucketWidth - 1).AverageGrade;
+            var cutoff_C = studentsOrderedByGrades.ElementAt(3*bucketWidth-1).AverageGrade;
+            var cutoff_D = studentsOrderedByGrades.ElementAt(4*bucketWidth-1).AverageGrade;
+            if (averageGrade >= cutoff_A) return 'A';
+            if (averageGrade >= cutoff_B) return 'B';
+            if (averageGrade >= cutoff_C) return 'C';
+            if (averageGrade >= cutoff_D) return 'D';
+            return 'F';
         }
     }
 }

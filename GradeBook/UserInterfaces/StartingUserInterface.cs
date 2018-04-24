50a0a9695b44b805e117714f47ecfcdd6b9ec262
dmu2014 <dishank2007@gmail.com>
2018-04-24 00:31:46 -0400

    Add prop

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -33,22 +33,23 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 3)
+            if (parts.Length != 4)
             {
-                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
+                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted (true / false).");
                 return;
             }
             var name = parts[1];
             var type = parts[2];
+            var isWeighted = Convert.ToBoolean(parts[3]);
             //BaseGradeBook gradeBook = new BaseGradeBook(name);
             BaseGradeBook gradeBook = null;
             if (type == "standard")
             {
-                gradeBook = new StandardGradeBook(name);
+                gradeBook = new StandardGradeBook(name, isWeighted);
             }
             if (type == "ranked")
             {
-                gradeBook = new RankedGradeBook(name);
+                gradeBook = new RankedGradeBook(name, isWeighted);
             }
             if (type != "standard" && type != "ranked")
             {
